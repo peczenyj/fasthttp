f5f059f3b58a43e14ea65f4466fbd0bbd21f74b6
Tiago Peczenyj <tpeczenyj@weborama.com>
2025-06-23 16:01:03 +0200

    substitute nocopy by pragma.DoNotCopy from protocolbuffers

--- nocopy.go
+++ nocopy.go
@@ -1,11 +1,11 @@
 package fasthttp
 
-// Embed this type into a struct, which mustn't be copied,
-// so `go vet` gives a warning if this struct is copied.
-//
-// See https://github.com/golang/go/issues/8005#issuecomment-190753527 for details.
-// and also: https://stackoverflow.com/questions/52494458/nocopy-minimal-example
-type noCopy struct{}
+import "sync"
 
-func (*noCopy) Lock()   {}
-func (*noCopy) Unlock() {}
+// this is a subset of the internal pragma package from github.com/protocolbuffers/protobuf-go
+// see https://github.com/protocolbuffers/protobuf-go/blob/32018e9a48fe1fb3addef57cd8b745ade54fc0d7/internal/pragma/pragma.go#L29
+// This does not rely on a Go language feature, but rather a special case
+// within the vet checker.
+//
+// See https://golang.org/issues/8005.
+type noCopy [0]sync.Mutex
